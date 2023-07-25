bc17e506f96a1043197b6b26e76b08325f42932d
jeremymanning <jeremy.r.manning@dartmouth.edu>
2023-07-25 15:14:45 -0400

    fix headings issue

--- chatify/process_notebooks.py
+++ chatify/process_notebooks.py
@@ -21,9 +21,7 @@ CACHE = False
 
 
 def get_tutorial_notebooks(basedir):
-    return lsdir(os.path.join(basedir, 'tutorials', '*', 'student', '*Tutorial*.ipynb')) + \
-           lsdir(os.path.join(basedir, 'tutorials', '*', 'instructor', '*Tutorial*.ipynb')) + \
-           lsdir(os.path.join(basedir, 'tutorials', '*', '*Tutorial*.ipynb'))
+    return lsdir(os.path.join(basedir, 'tutorials', '*', '*Tutorial*.ipynb'))
 
 
 def chatified(fname):
